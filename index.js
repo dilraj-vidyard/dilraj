3e77b1ca51c121fdfa638981f9468a17e794b0ed
Dilraj Vidyard <dilraj.singh@vidyard.com>
2022-12-10 17:48:06 -0500

    Testing CLI

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+console.log(`Dilraj Singh
+
+Software Developer at Vidyard
+
+The number ⭐️ one most impactful developer of our generation.
+
+👨‍💻 GitHub: https://github.com/dsomel21
+🐦 Twitter: https://twitter.com/dilrajio
+🫂  Best Friend at Vidyard: Matt Cook
+
+`)
